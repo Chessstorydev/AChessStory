821a2950c41527bf23cd24e85ba8406b80535fa0
treem0 <38818872+treem0@users.noreply.github.com>
2018-05-02 21:19:12 -0700

    Added menu
    Created new method that prints the menu

--- src/com/ACS/TestProgram.java
+++ src/com/ACS/TestProgram.java
@@ -1,7 +1,6 @@
 package com.ACS;
 
 
-
 import java.util.Scanner;
 
 public class TestProgram {
@@ -9,7 +8,27 @@ public class TestProgram {
 
     public static void main(String[] args) {
         // write your code here
+Scanner inputScanner = new Scanner(System.in);
+printMenu();
+int myIntiger = inputScanner.nextInt();
+
+//int myIntiger = 5;
+//        System.out.println("Hello World");
+      System.out.println(myIntiger);
+//myIntiger = 7;
+//        System.out.print(myIntiger);
+
     }
 
+
+    public static void printMenu()
+    {
+        System.out.println("Welcome to the UI for the Chess Program");
+        System.out.println("1: Display the Game");
+        System.out.println("2: Make a Move");
+        System.out.println("Please Make a Choice");
+
+
+    }
 
 }
